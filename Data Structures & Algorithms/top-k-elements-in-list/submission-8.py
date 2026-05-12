cefbd591efbb6359e74d615baecabe12305e7e4a
Kavya Gupta <kavyagupta505@gmail.com>
2026-05-12 20:51:43 +0530

    Add: top-k-elements-in-list - submission-8

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-8.py
@@ -0,0 +1,11 @@
+import itertools
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count_dict = {}
+        for num in nums:
+            count_dict[num] = count_dict.get(num, 0) + 1
+        output = []
+        sorted_dict = dict(sorted(count_dict.items(), key=lambda item: item[1], reverse=True))
+        for key in itertools.islice(sorted_dict, k):
+            output.append(key)
+        return output 
