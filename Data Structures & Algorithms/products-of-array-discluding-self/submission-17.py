6b31acdd26e125fde86738780ccbbe213e180b6d
Kavya Gupta <kavyagupta505@gmail.com>
2026-05-25 10:56:47 +0530

    Add: products-of-array-discluding-self - submission-17

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-17.py
@@ -0,0 +1,18 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        prefix = 1
+        suffix = 1
+        out = []
+        n = len(nums)
+        for i in range(n):
+            out.append(prefix)
+            prefix *= nums[i]
+        for i in range(n-1,-1,-1):
+            out[i] *= suffix
+            suffix *= nums[i]
+        return out
+'''
+In this solution I first created a single output array\list which firstly had prefixes
+and then i iterated from right to left while multiplying every element 
+at the correct index
+'''
