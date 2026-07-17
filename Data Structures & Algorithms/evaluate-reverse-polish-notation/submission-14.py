0d7a32aaacbe02bc0015f30827f37b6b6b4fea9f
Kavya Gupta <kavyagupta505@gmail.com>
2026-07-17 11:55:52 +0530

    Add: evaluate-reverse-polish-notation - submission-14

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-14.py
@@ -0,0 +1,24 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        operands = []
+        operators = ["+","-","*","/"]
+        for t in tokens:
+            res = 0
+            if t not in operators:
+                operands.append(int(t))
+            else:
+                a = operands.pop()
+                b = operands.pop()
+                if t == "+":
+                    operands.append(a+b)
+                elif t == "-":
+                    operands.append(b - a)
+                elif t== "*":
+                    operands.append(a * b)
+                else:
+                    try:
+                        operands.append(int(b/a))
+                    except ZeroDivisionError:
+                        print("Error: You cannot divide by zero.")
+                        res = None
+        return int(operands[0])
